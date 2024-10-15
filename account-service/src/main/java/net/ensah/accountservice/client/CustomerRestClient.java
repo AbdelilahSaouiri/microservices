a5523e2d83b8d059912ea3712cb0f4fd5cd4fbf5
abdelilah <abdelilah.saouir@etu.uae.ac.ma>
2024-10-15 15:27:38 +0100

    changes configurations

--- account-service/src/main/java/net/ensah/accountservice/client/CustomerRestClient.java
+++ account-service/src/main/java/net/ensah/accountservice/client/CustomerRestClient.java
@@ -2,6 +2,8 @@ package net.ensah.accountservice.client;
 
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import net.ensah.accountservice.model.Customer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,15 +13,18 @@ import java.util.List;
 @FeignClient(name="CUSTOMER-SERVICE")
 public interface CustomerRestClient {
 
+    Logger logger= LoggerFactory.getLogger(CustomerRestClient.class);
+
     @GetMapping("/api/customers/{customerId}")
-    @CircuitBreaker(name = "customerSerive",fallbackMethod = "getDefaultCustomer")
+    @CircuitBreaker(name = "customerService", fallbackMethod = "getDefaultCustomer")
     Customer getCustomerByCustomerId(@PathVariable String customerId);
 
     @GetMapping("/api/customers")
-    @CircuitBreaker(name = "ListCustomerSerivce",fallbackMethod = "getAllCustomers")
+    @CircuitBreaker(name = "listCustomerService", fallbackMethod = "getAllCustomers")
     List<Customer> getCustomers();
 
-    default Customer getDefaultCustomer(String customerId) {
+    default Customer getDefaultCustomer(String customerId, Throwable throwable) {
+        logger.error("Fallback for getCustomerByCustomerId due to: {}", throwable.getMessage());
         return Customer.builder()
                 .customerId(customerId)
                 .firstname("default")
@@ -28,7 +33,8 @@ public interface CustomerRestClient {
                 .build();
     }
 
-    default List<Customer> getAllCustomers() {
+    default List<Customer> getAllCustomers(Throwable throwable) {
+        logger.error("Fallback for getCustomers due to: {}", throwable.getMessage());
         return List.of();
     }
 }
--- customer-service/src/main/java/net/ensah/customerservice/controller/ConfigTestController.java
+++ customer-service/src/main/java/net/ensah/customerservice/controller/ConfigTestController.java
@@ -1,38 +1,40 @@
-package net.ensah.customerservice.controller;
-
-import net.ensah.customerservice.config.GlobalConfig;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.HashMap;
-import java.util.Map;
-
-@RestController
-@RequestMapping("/config")
-public class ConfigTestController {
-
-    @Value("${global.params.x}")
-    private int x;
-    @Value("${global.params.y}")
-    private int y;
-
-    @Autowired
-    private GlobalConfig globalConfig;
-
-    @GetMapping
-    public Map<String, String> getConfig() {
-       Map<String, String> map = new HashMap<>();
-       map.put("x", String.valueOf(x));
-       map.put("y", String.valueOf(y));
-       return map;
-    }
-
-    @GetMapping("/global")
-    public GlobalConfig getGlobalConfig() {
-        return globalConfig;
-    }
-
-}
+//package net.ensah.customerservice.controller;
+//
+//import net.ensah.customerservice.config.GlobalConfig;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.cloud.context.config.annotation.RefreshScope;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import java.util.HashMap;
+//import java.util.Map;
+//
+//@RestController
+//@RequestMapping("/config")
+//@RefreshScope
+//public class ConfigTestController {
+//
+//    @Value("${customer.params.x}")
+//    private Long x;
+//    @Value("${customer.params.y}")
+//    private Long y;
+//
+//    @Autowired
+//    private GlobalConfig globalConfig;
+//
+//    @GetMapping
+//    public Map<String, String> getConfig() {
+//       Map<String, String> map = new HashMap<>();
+//       map.put("x", String.valueOf(x));
+//       map.put("y", String.valueOf(y));
+//       return map;
+//    }
+//
+//    @GetMapping("/global")
+//    public GlobalConfig getGlobalConfig() {
+//        return globalConfig;
+//    }
+//
+//}
